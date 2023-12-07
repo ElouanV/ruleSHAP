b053be299664e70af44ff0cf800ae3fd1ef2e091
Elouan Vincent <elouan.vincent@epita.fr>
2023-12-07 15:48:56 +0100

    tt

--- src/run_shapley_rule.py
+++ src/run_shapley_rule.py
@@ -12,9 +12,41 @@ from explainers.shapley_rule import ShapleyRule, KernelShapRule
 from models.gnnNets import get_gnnNets
 from utils import parse_rules
 import numpy as np
+from tqdm import tqdm
 
 
-def progressive_rule_score(model, dataset, dataloader, activation_rules, targeted_class=0):
+def simple_rule_score(model, dataset, dataloader, activation_rules, k=100):
+    print(f'COmputing simple rule score for k={k}')
+    df_result = pd.DataFrame(columns=['rule_layer', 'rule_vector', 'rule_target', 'contribution_class_0',
+                                      'contribution_class_1', 'exact_shapley_class_0', 'exact_shapley_class_1'])
+    estimator = KernelShapRule('ba2', model, dataset, dataloader, activation_rules, targeted_class=0,
+                               strategy='deactivate')
+    start = time.time()
+    estimator.add_sampled_coalition(k)
+    estimator.fit()
+    values = estimator.get_shapley_values()
+    end = time.time()
+    print(f'\nTook {(end - start):.3f} seconds \n')
+    for i, values in enumerate(values):
+        df_result.loc[i, 'contribution_class_0'] = values
+
+    estimator = KernelShapRule('ba2', model, dataset, dataloader, activation_rules, targeted_class=1,
+                               strategy='deactivate')
+    start = time.time()
+    estimator.add_sampled_coalition(k)
+    estimator.fit()
+    values = estimator.get_shapley_values()
+    end = time.time()
+    # Rounded to 3 decimals the time taken
+    print(f'\nTook {(end - start):.3f} seconds \n')
+    for i, values in enumerate(values):
+        df_result.loc[i, 'contribution_class_1'] = values
+    df_result.to_csv('./ExplanationEvaluation/ex_ba_2motifs_kernel_shapley_rule.csv', index=False)
+    print(df_result)
+    return df_result
+
+
+def progressive_rule_score(model, dataset, dataloader, activation_rules, targeted_class=0, sampling=50):
     """
     Compute the progressive rule score for each rule in the activation_rules list
     :param model: GNN model
@@ -25,42 +57,30 @@ def progressive_rule_score(model, dataset, dataloader, activation_rules, targete
     :return: list of progressive rule scores
     """
     progressive_rule_scores = []
-    for cls in range(2):
-        estimator = KernelShapRule('ba2', model, dataset, dataloader, activation_rules, targeted_class=targeted_class)
-        for _ in range(10):
-            estimator.add_sampled_coalition(100)
-            estimator.fit()
-            progressive_rule_scores.append(estimator.get_shapley_values())
+    estimator = KernelShapRule('ba2', model, dataset, dataloader, activation_rules, targeted_class=targeted_class)
+    for _ in tqdm(range(sampling // 100)):
+        estimator.add_sampled_coalition(100)
+        estimator.fit()
+        progressive_rule_scores.append(estimator.get_shapley_values())
     return progressive_rule_scores
 
 
-@hydra.main(config_path="config", config_name="config")
+
+
+
+@hydra.main(version_base=None, config_path="config", config_name="config")
 def main(config):
     config.models.gnn_saving_path = os.path.join(
         hydra.utils.get_original_cwd(), config.models.gnn_saving_path
     )
     config.models.param = config.models.param[config.datasets.dataset_name]
-    # print(OmegaConf.to_yaml(config))
-
-    if torch.cuda.is_available():
-        device = torch.device("cuda", index=config.device_id)
-        print('Using GPU')
-    else:
-        device = torch.device("cpu")
-
     dataset = get_dataset(
         dataset_root=config.datasets.dataset_root,
         dataset_name=config.datasets.dataset_name,
     )
     dataset.data.x = dataset.data.x.float()
     dataset.data.y = dataset.data.y.squeeze().long()
-    if config.models.param.graph_classification:
-        dataloader_params = {
-            "batch_size": config.models.param.batch_size,
-            "random_split_flag": config.datasets.random_split_flag,
-            "data_split_ratio": config.datasets.data_split_ratio,
-            "seed": config.datasets.seed,
-        }
+
     print(dataset)
     print(max([d.num_nodes for d in dataset]))
     # Get only train set
@@ -69,65 +89,95 @@ def main(config):
     pretrained_model = torch.load(
         config.models.gnn_saving_path + "/" + f'{config.datasets.dataset_name}/{config.models.gnn_name}_3l_best.pth')
     model.load_state_dict(pretrained_model['net'])
-    dataloader = get_data_loader(dataset, config.models.param.batch_size, config.datasets.random_split_flag,
+    dataloader = get_data_loader(dataset, 1024, config.datasets.random_split_flag,
                                  config.datasets.data_split_ratio, config.datasets.seed)
 
-    activation_rules = parse_rules('./ExplanationEvaluation/ba_2motifs_encode_motifs.csv', emb_size=16)
-    # Sample 8 activation rules
+    activation_rules = parse_rules('ExplanationEvaluation/ba_2motifs_encode_motifs.csv', emb_size=16)
+    print(f'Activation rules length: {len(activation_rules)}')
+    """
     df_result = pd.DataFrame(columns=['rule_layer', 'rule_vector', 'rule_target', 'contribution_class_0',
                                       'contribution_class_1', 'exact_shapley_class_0', 'exact_shapley_class_1'])
-    for i, (layer, vector, target) in enumerate(activation_rules):
+    for i, (layer, vector, target, _, _, _) in enumerate(activation_rules):
         df_result.loc[i] = [layer, vector, target, 0, 0, 0, 0]
-    for layer, vector, target in activation_rules:
+    for layer, vector, target, _, _, _ in activation_rules:
         print(f'Layer: {layer}, Vector: {vector}, Target: {target}')
 
+    df = simple_rule_score(model, dataset, dataloader, activation_rules, k=100)
 
-    ########### Apply Kernel Shapley Rule #############
-    k = 5000
-    estimator = KernelShapRule('ba2', model, dataset, dataloader, activation_rules, targeted_class=0, k =k, strategy='replace')
-    start = time.time()
-    estimator.fit()
-    values = estimator.get_shapley_values()
-    end = time.time()
-    print(f'\nTook {(end - start):.3f} seconds \n')
-    for i, values in enumerate(values):
-        df_result.loc[i, 'contribution_class_0'] = values
+    # Add rule from activation_rules to the df
+    df['rule_inside_score'] = [val[3] for val in activation_rules]
+    df['rule_inside_score_c0'] = [val[4] for val in activation_rules]
+    df['rule_inside_score_c1'] = [val[5] for val in activation_rules]
 
-    estimator = KernelShapRule('ba2', model, dataset, dataloader, activation_rules, targeted_class=1, k=k, strategy='replace')
-    start = time.time()
-    estimator.fit()
-    values = estimator.get_shapley_values()
-    end = time.time()
-    # Rounded to 3 decimals the time taken
-    print(f'\nTook {(end - start):.3f} seconds \n')
-    for i, values in enumerate(values):
-        df_result.loc[i, 'contribution_class_1'] = values
-    df_result.to_csv('./ExplanationEvaluation/ex_ba_2motifs_kernel_shapley_rule.csv', index=False)
-    print(df_result)
+    df.to_csv('./ExplanationEvaluation/ex_ba_2motifs_kernel_shapley_rule.csv', index=False)
+    print(df)
 
-    # Plot the values for each rule with the rule_target as color
-
-    # Plot rule that target class 0
-    df_target_0 = df_result[df_result['rule_target'] == 0]
-    plt.scatter(df_target_0['contribution_class_0'], df_target_0['contribution_class_1'], c='r', label='Target 0')
-    # plt.scatter(df_target_0['exact_shapley_class_0'], df_target_0['exact_shapley_class_1'], c='r', marker='x', label='Exact Shapley')
-    # Plot rule that target class 1
-    df_target_1 = df_result[df_result['rule_target'] == 1]
-    plt.scatter(df_target_1['contribution_class_0'], df_target_1['contribution_class_1'], c='y', label='Target 1')
-    # plt.scatter(df_target_1['exact_shapley_class_0'], df_target_1['exact_shapley_class_1'], c='y', marker='x', label='Exact Shapley')
-
-
-    lim = np.max(np.array([float(np.max(np.abs(df_result['contribution_class_0'].values))), float(np.max(np.abs(df_result['contribution_class_1'].values)))]))
-    print(f'Lim: {lim}')
-    plt.xlim(-lim, lim)
-    plt.ylim(-lim, lim)
+    # Plot the values for each rule with the rule_target as color according to the rule_inside_score
+    plt.scatter(df['contribution_class_0'], df['contribution_class_1'], c=df['rule_inside_score'], cmap='viridis')
+    plt.colorbar()
     plt.xlabel('Contribution to class 0')
     plt.ylabel('Contribution to class 1')
-    plt.title(f'Estimated Shapley values for each rule, k={k}')
-    plt.savefig('./ExplanationEvaluation/ex_ba_2motifs_kernel_shapley_rule.png')
+    plt.title(f'Estimated Shapley values for each rule, k=100')
+    plt.savefig('./ExplanationEvaluation/ex_ba2_contrib_score_c_k100.png')
+
+    # Plot the values for each rule with the rule_target as color according to the rule_inside_score_c0
+    plt.scatter(df['contribution_class_0'], df['contribution_class_1'], c=df['rule_inside_score_c0'], cmap='viridis')
+    plt.colorbar()
+    plt.xlabel('Contribution to class 0')
+    plt.ylabel('Contribution to class 1')
+    plt.title(f'Estimated Shapley values for each rule, k=100')
+    plt.savefig('./ExplanationEvaluation/ex_ba2_contrib_score_c0_k100.png')
+
+    # Plot the values for each rule with the rule_target as color according to the rule_inside_score_c1
+    plt.scatter(df['contribution_class_0'], df['contribution_class_1'], c=df['rule_inside_score_c1'], cmap='viridis')
+    plt.colorbar()
+    plt.xlabel('Contribution to class 0')
+    plt.ylabel('Contribution to class 1')
+    plt.title(f'Estimated Shapley values for each rule, k=100')
+    plt.savefig('./ExplanationEvaluation/ex_ba2_contrib_score_c1_k100.png')"""
+    k = 100000
+    # Call progressive_rule_score for each class on k = 100 000
+    progressive_rule_scores_c0 = progressive_rule_score(model, dataset, dataloader, activation_rules, targeted_class=0,
+                                                        sampling=k)
+    progressive_rule_scores_c1 = progressive_rule_score(model, dataset, dataloader, activation_rules, targeted_class=1,
+                                                        sampling=k)
+    # Save the results
+    np.save('./ExplanationEvaluation/ex_ba2_progressive_rule_scores_c0_k100000.npy', progressive_rule_scores_c0)
+    np.save('./ExplanationEvaluation/ex_ba2_progressive_rule_scores_c1_k100000.npy', progressive_rule_scores_c1)
+
+    # Plot evolution of the progressive rule score for each rule by computing mean for each class
+    progressive_rule_scores_c0 = np.load('./ExplanationEvaluation/ex_ba2_progressive_rule_scores_c0_k100000.npy',
+                                         allow_pickle=True)
+    progressive_rule_scores_c1 = np.load('./ExplanationEvaluation/ex_ba2_progressive_rule_scores_c1_k100000.npy',
+                                         allow_pickle=True)
+    progressive_rule_scores_c0 = np.mean(progressive_rule_scores_c0, axis=0)
+    progressive_rule_scores_c1 = np.mean(progressive_rule_scores_c1, axis=0)
+    plt.plot(progressive_rule_scores_c0, label='Class 0')
+    plt.plot(progressive_rule_scores_c1, label='Class 1')
+    plt.xlabel('Number of sampled coalitions')
+    plt.ylabel('Progressive rule score')
+    plt.title(f'Progressive rule score for each rule, k=100 000')
     plt.legend()
+    plt.savefig(f'./ExplanationEvaluation/ex_ba2_progressive_rule_score_k1{100000}.png')
     plt.show()
 
+    # Do the same with standard deviation
+    progressive_rule_scores_c0 = np.load('./ExplanationEvaluation/ex_ba2_progressive_rule_scores_c0_k100000.npy',
+                                            allow_pickle=True)
+    progressive_rule_scores_c1 = np.load('./ExplanationEvaluation/ex_ba2_progressive_rule_scores_c1_k100000.npy',
+                                            allow_pickle=True)
+    progressive_rule_scores_c0 = np.std(progressive_rule_scores_c0, axis=0)
+    progressive_rule_scores_c1 = np.std(progressive_rule_scores_c1, axis=0)
+
+    plt.plot(progressive_rule_scores_c0, label='Class 0')
+    plt.plot(progressive_rule_scores_c1, label='Class 1')
+    plt.xlabel('Number of sampled coalitions')
+    plt.ylabel('Progressive rule score')
+    plt.title(f'Progressive rule score for each rule, k=100 000')
+    plt.legend()
+    plt.savefig(f'./ExplanationEvaluation/ex_ba2_progressive_rule_score_std_k1{100000}.png')
+    plt.show()
+    return None
 
 if __name__ == "__main__":
     main()
